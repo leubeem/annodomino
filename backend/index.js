1b88bdf508fe5242a31ee95a2afedeb5733c2d70
Vserver1 | Emanuel Leube <emanuel@binder-leube.de>
2021-10-21 18:05:48 +0000

    Added backend to insert data, fixed multiple bugs, add some things

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var mysql = require("mysql");
 var bodyParser = require('body-parser');
+var fs = require('fs');
+var http = require('http')
 var app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
@@ -22,14 +24,24 @@ app.get('/milestone', function (req,res) {
 				//throw err;
 				console.log(error);	
 				res.status(503);
-				res.end("Error, probably at inserting data into database");
+				res.json({'message':"Error at getting data from database"});
+				res.end();
 			} else {
 				//returning the rows as json
 				console.log("Returning rows from databse to requester");
 				res.json(result);
 			}
-		});
-	});
+		}
+	);
+});
+
+//Implementing insert data
+app.get('/add', function (req,res) {
+	//If add is called, serve static add.html file with form to enter data
+	res.status(200);
+	res.set({'Content-Type':'text/html'});
+	fs.createReadStream('add.html').pipe(res);
+});
 
 //Implementing post features for api
 app.post('/milestone', function(req,res) {
@@ -41,20 +53,29 @@ app.post('/milestone', function(req,res) {
 	const title = reqBody.title;
 	const categoryId = reqBody.categoryId;
 
-	//query to inser post values into database
-	const query = `Insert into questions (year, title, category) VALUES (?,?,?);`;
-	con.query(query, [year, title, categoryId], function (err, data) {
-		if(err) {
-			//throw err;
-			console.log(error);
-			res.status(503);
-			res.end("Error, probably at inserting data into database");
-		} else {
-			//returning success and 202 OK
-			console.log("Added row to database");
-			res.end('Success!');
-		}
-	});
+		//Params mustn't be null
+	if (year && title && categoryId) {
+		//query to inser post values into database
+		const query = `Insert into questions (year, title, category) VALUES (?,?,?);`;
+		con.query(query, [year, title, categoryId], function (err, data) {
+			if(err) {
+				//throw err;
+				console.log(error);
+				res.status(503)
+				res.json({'message':"Error, probably at inserting data into database"})
+				res.end()
+			} else {
+				//returning success and 202 OK
+				console.log("Added row to database");
+				res.end('Success!');
+			}
+		});
+	} else {
+		console.error("Params mustn't be null")
+		res.status(503)
+		res.json({'message':'Empty parameters aren\'t allowed!'})
+		res.end()
+	}
 })
 
 //Starting server and listening on port 8081
